2e2f953eccfb5a1fdf999986d0e9218335d0f2cf
mcclean <randolphmcclean@gmail.com>
2014-01-02 21:00:56 -0500

    Created home screen + custom listItem

--- MIA/gen/com/libratech/mia/R.java
+++ MIA/gen/com/libratech/mia/R.java
@@ -24,11 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int ribbonMenuView=0x7f080000;
+        public static final int ScanProgressText=0x7f080003;
+        public static final int action_settings=0x7f08000c;
+        public static final int itemBrand=0x7f080009;
+        public static final int itemName=0x7f080008;
+        public static final int itemNum=0x7f080007;
+        public static final int itemPrice=0x7f08000a;
+        public static final int itemScanned=0x7f08000b;
+        public static final int listItem=0x7f080006;
+        public static final int mainlistview=0x7f080004;
+        public static final int ribbonMenuView=0x7f080005;
+        public static final int scanProgressBar=0x7f080000;
+        public static final int scannedButton=0x7f080001;
+        public static final int unScannedButton=0x7f080002;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
+        public static final int row=0x7f030001;
     }
     public static final class menu {
         public static final int home=0x7f070000;
