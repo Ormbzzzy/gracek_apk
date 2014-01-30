26ae88cc8ddec8a683020e5ef1524bc02f085e08
Matthew Hall <maseu21@gmail.com>
2014-01-30 17:36:06 -0500

    fix

--- MIA/gen/com/libratech/mia/R.java
+++ MIA/gen/com/libratech/mia/R.java
@@ -214,11 +214,10 @@ public final class R {
         public static final int standard_padding=0x7f090000;
     }
     public static final class drawable {
-        public static final int divider=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int launcher_icon=0x7f020002;
-        public static final int share_via_barcode=0x7f020003;
-        public static final int shopper_icon=0x7f020004;
+        public static final int ic_launcher=0x7f020000;
+        public static final int launcher_icon=0x7f020001;
+        public static final int share_via_barcode=0x7f020002;
+        public static final int shopper_icon=0x7f020003;
     }
     public static final class id {
         public static final int Category=0x7f0a0049;
